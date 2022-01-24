44a47b83393416abfce86cfd5a04deb9fcc0d4b0
Erica <ericafabs@gmail.com>
2022-01-24 21:36:10 +0000

    hopefully the final fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': '2themoon',
+        'USER': 'djangodbman',
+        'PASSWORD': '2Themoon',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -137,7 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
 
