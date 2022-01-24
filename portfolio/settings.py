2f2e683a4ec7b8574ac496338e38c8b9029c1116
Erica Fabian <ericafabs@gmail.com>
2022-01-24 15:46:36 -0800

    password change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '2Themoon',
+        'PASSWORD': '2themoon',
         'HOST': 'localhost',
         'PORT': '5432',
     }
